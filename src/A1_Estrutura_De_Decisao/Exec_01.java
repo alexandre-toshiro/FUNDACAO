b92bbbc62a213ab23352c773123f0f5a95f56e65
Toshiro <toshiro.lyn@gmail.com>
2020-06-22 19:47:23 -0300

    Starting

---
+++ src/A1_Estrutura_De_Decisao/Exec_01.java
@@ -0,0 +1,10 @@
+package A1_Estrutura_De_Decisao;
+
+public class Exec_01 {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
---
+++ src/A1_Estrutura_De_Decisao/Exec_02.java
@@ -0,0 +1,10 @@
+package A1_Estrutura_De_Decisao;
+
+public class Exec_02 {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
